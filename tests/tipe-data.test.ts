aa4f347c736e477eca121f8d1a76d2c1183f911f
choiruladamm <choiruladamm@gmail.com>
2023-07-09 12:41:21 +0700

    Tipe Data Primitif / Data Type

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe("Data Type", () => {
+  it("must declare", () => {
+    const name: string = "Choirul Adamm";
+    const balance: number = 10000;
+    const isVip: boolean = false;
+
+    console.info(name);
+    console.info(balance);
+    console.info(isVip);
+  });
+});
